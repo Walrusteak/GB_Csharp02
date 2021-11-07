c250cabaa4998247a5af08635b2b8cdc1204d597
Vladislav Yagello <y.surprise@gmail.com>
2021-11-07 20:42:23 +0300

    Homework03

---
+++ Homework03/Task01/BenchmarkClass.cs
@@ -0,0 +1,64 @@
+﻿using BenchmarkDotNet.Attributes;
+namespace Task01
+{
+    public class BenchmarkClass
+    {
+        [Benchmark]
+        public void TestFloatClass()
+        {
+            PointFloatClass.PointDistance(new PointFloatClass() { X = 10, Y = 50 }, new PointFloatClass() { X = -50, Y = 100 });
+        }
+
+        [Benchmark]
+        public void TestFloatStruct()
+        {
+            PointFloatStruct.PointDistance(new PointFloatStruct() { X = 10, Y = 50 }, new PointFloatStruct() { X = -50, Y = 100 });
+        }
+
+        [Benchmark]
+        public void TestFloatStructShort()
+        {
+            PointFloatStruct.PointDistanceShort(new PointFloatStruct() { X = 10, Y = 50 }, new PointFloatStruct() { X = -50, Y = 100 });
+        }
+
+        [Benchmark]
+        public void TestDoubleStruct()
+        {
+            PointDoubleStruct.PointDistance(new PointDoubleStruct() { X = 10, Y = 50 }, new PointDoubleStruct() { X = -50, Y = 100 });
+        }
+    }
+
+    public class BenchmarkClassPreCreated
+    {
+        PointFloatClass pfc1 = new() { X = 10, Y = 50 };
+        PointFloatClass pfc2 = new() { X = -50, Y = 100 };
+        PointFloatStruct pfs1 = new() { X = 10, Y = 50 };
+        PointFloatStruct pfs2 = new() { X = -50, Y = 100 };
+        PointDoubleStruct pds1 = new() { X = 10, Y = 50 };
+        PointDoubleStruct pds2 = new() { X = -50, Y = 100 };
+
+        [Benchmark]
+        public void TestFloatClass()
+        {
+            PointFloatClass.PointDistance(pfc1, pfc2);
+        }
+
+        [Benchmark]
+        public void TestFloatStruct()
+        {
+            PointFloatStruct.PointDistance(pfs1, pfs2);
+        }
+
+        [Benchmark]
+        public void TestFloatStructShort()
+        {
+            PointFloatStruct.PointDistanceShort(pfs1, pfs2);
+        }
+
+        [Benchmark]
+        public void TestDoubleStruct()
+        {
+            PointDoubleStruct.PointDistance(pds1, pds2);
+        }
+    }
+}
---
+++ Homework03/Task01/PointDoubleStruct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Task01
+{
+    public struct PointDoubleStruct
+    {
+        public double X;
+        public double Y;
+
+        public static double PointDistance(PointDoubleStruct p1, PointDoubleStruct p2)
+        {
+            double x = p1.X - p2.X;
+            double y = p1.Y - p2.Y;
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
