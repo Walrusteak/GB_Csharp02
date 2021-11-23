cd7409bfb93b68276c909d08e64e58b0358df029
Vladislav Yagello <y.surprise@gmail.com>
2021-11-23 21:08:57 +0300

    Homework07

---
+++ Homework07/Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int m = 6;
+            const int n = 8;
+
+            int[,] field = new int[m, n];
+            for (int j = 0; j < n; j++)
+                field[0, j] = 1;
+            for (int i = 1; i < m; i++)
+            {
+                field[i, 0] = 1;
+                for (int j = 1; j < n; j++)
+                    field[i, j] = field[i, j - 1] + field[i - 1, j];
+            }
+
+            int padding = field[m - 1, n - 1].ToString().Length + 2;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(field[i, j].ToString().PadLeft(padding));
+                Console.WriteLine();
+            }
+        }
+    }
+}
